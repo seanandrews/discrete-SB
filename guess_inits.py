b1c546533772cfce9c7f2a13a696f49db0c797d6
Sean Andrews <sandrews@cfa.harvard.edu>
2015-07-24 15:12:53 -0400

    Scaled surface brightnesses, compare to the known truth

--- guess_inits.py
+++ guess_inits.py
@@ -26,11 +26,10 @@ RAo, DECo = np.meshgrid(RA-RA_cen, DEC-DEC_cen)
 
 # radial profile
 radius = 3600.*np.sqrt(RAo**2 + DECo**2)
-plt.loglog(radius, data_image, '.r')
 
 # bins averages
-nbins = 40
-b = np.linspace(0.1, 2.0, num=nbins)
+nbins = 20
+b = 0.05 + 0.05*np.arange(nbins)
 a = np.roll(b, 1)
 rin = 0.01/140.
 a[0] = rin
@@ -38,13 +37,22 @@ cb = 0.5*(a+b)
  
 # averge surface brightness in each bin
 avg_sb = np.zeros_like(cb)
+bareas = np.zeros_like(cb)
 for i in range(nbins):
     avg_sb[i] = np.mean(data_image[(radius > a[i]) & (radius < b[i])])
+    bareas[i] = np.pi*(b[i]**2-a[i]**2)
 
-plt.loglog(cb, avg_sb, 'ob')
+# scale to units of Jy per square arcsec
+omega_beam = np.pi*(3600.**2)*hdr['BMAJ']*hdr['BMIN']/(4.*np.log(2.))
+avg_sb /= omega_beam
+
+# the truth!
+rtruth  = np.logspace(-2, 0.1, num=100)
+rc = 0.39285714
+Ic = 0.0824975
+SBtruth = Ic * (rc/rtruth)
+SBtruth[rtruth > rc] = Ic * (rtruth[rtruth > rc]/rc)**(-4.)
+
+plt.axis([0.01, 1.5, 1e-4, 1e1])
+plt.loglog(rtruth, SBtruth, '-k', cb, avg_sb, 'ob')
 plt.savefig('profile.png') 
-
-
-#plt.figure()
-#plt.imshow(data_image, origin='lower', cmap='bone_r')
-#plt.show()
